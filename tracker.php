553e2478e895ebc9d14b44e37f275b511f74e65d
AaronKurowski <aaron12342@att.net>
2021-11-18 21:36:11 -0600

    Basic table and styles

---
+++ tracker.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta>
+        <title>Raid Tracker</title>
+
+        <link rel="stylesheet" href="styles.css">
+        <script>
+
+        </script>
+    </head>
+
+    <body>
+        <div id="main-wrapper">
+            <table>
+                <thead>
+                    <!-- These Table headers need to be changeable and populated from the accociated array coming back-->
+                    <tr>
+                        <th>ONE</th>
+                        <th>TWO</th>
+                        <th>THREE</th>
+                        <th>FOUR</th>
+                        <th>FIVE</th>
+                        <th>SIX</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A</td>
+                        <td>B</td>
+                        <td>K</td>
+                        <td>D</td>
+                        <td>D</td>
+                        <td>T</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
